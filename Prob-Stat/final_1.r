884ba06e38fdf523a188e39031578735ac4d3d58
Advait Sepuri <90327626+adsepuri35@users.noreply.github.com>
2024-12-06 14:19:54 -0500

    Create final_1.r

---
+++ Prob-Stat/final_1.r
@@ -0,0 +1,42 @@
+# Set parameters
+set.seed(123)   # For reproducibility
+n <- 20         # Sample size
+num_sim <- 100000   # Number of simulations
+sigma_epsilon <- 2 # Standard deviation (sqrt(4))
+
+# Generate X values
+x <- 1:n
+
+# Coefficients
+alpha1 <- 2
+alpha2 <- 1
+alpha3 <- 1
+alpha4 <- 2
+
+# Initialize a vector to store sums of estimates
+sum_alpha_hat <- numeric(num_sim)
+
+for (sim in 1:num_sim) {
+  # Generate random errors
+  epsilon <- rnorm(n, mean = 0, sd = sigma_epsilon)
+  
+  # Generate Y values
+  Y <- alpha1 + alpha2 * x + alpha3 * x^2 + alpha4 * log(x) + epsilon
+  
+  # Fit the model
+  fit <- lm(Y ~ x + I(x^2) + log(x))
+  
+  # Sum of coefficients
+  sum_alpha_hat[sim] <- sum(coef(fit))
+}
+
+# Plot the approximated PDF
+hist(sum_alpha_hat, breaks = 100, probability = TRUE, main = "Approximated PDF of Sum of Coefficients", xlab = "Sum of Estimates")
+lines(density(sum_alpha_hat), col = "blue")
+
+# Approximate mean and variance
+mean_est <- mean(sum_alpha_hat)
+var_est <- var(sum_alpha_hat)
+
+mean_est
+var_est

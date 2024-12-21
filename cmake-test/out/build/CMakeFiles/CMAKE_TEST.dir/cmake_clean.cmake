f189acfac753734a477ce2259812112bf7120901
Advait Sepuri <90327626+adsepuri35@users.noreply.github.com>
2024-12-20 20:01:48 -0500

    tutorial 1

---
+++ cmake-test/out/build/CMakeFiles/CMAKE_TEST.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMAKE_TEST"
+  "CMAKE_TEST.pdb"
+  "CMakeFiles/CMAKE_TEST.dir/main.cpp.o"
+  "CMakeFiles/CMAKE_TEST.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMAKE_TEST.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
